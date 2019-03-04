0f6bb060b1193a84e55d31afcd0cfb77af0d534b
duangduangda <dongyhemail@sina.com>
2019-03-04 23:51:18 +0800

    flink local runner

---
+++ src/main/java/org/dean/flink/LocalRunner.java
@@ -0,0 +1,28 @@
+package org.dean.flink;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+public class LocalRunner {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment executionEnvironment = ExecutionEnvironment.createCollectionsEnvironment();
+        // create a dataset of numbers
+        DataSet<Integer> numbers = executionEnvironment.fromElements(1, 2, 3 ,4 , 5, 6,7,8,9,10);
+        // square every  number
+        DataSet<Integer> result = numbers.map(new MapFunction<Integer, Integer>() {
+            public Integer map(Integer value) throws Exception {
+                return value * value;
+            }
+        }).filter(new FilterFunction<Integer>() {
+            public boolean filter(Integer value) throws Exception {
+                return value % 2 != 0;
+            }
+        });
+
+        result.print();
+
+
+    }
+}
